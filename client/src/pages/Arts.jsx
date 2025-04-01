e463803b512f88f70bf148409aa5d0403ee7cb2b
Tashi <tashik507@gmail.com>
2025-04-01 11:42:22 +0600

    UI changes =

--- client/src/pages/Arts.jsx
+++ client/src/pages/Arts.jsx
@@ -3,133 +3,149 @@ import Sidebar from "../components/Sidebar";
 
 function Arts() {
   return (
-    <div className="d-flex">
+    <div className="wrapper">
       <Sidebar />
-      <div>
-        <h1 className="d-block mx-auto mt-2">Class XII Arts</h1>
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <h1 className="d-block mx-auto mt-2 mb-3 fs-2">Class XII Arts</h1>
         <form className="marksheet-form">
-          <div className="form-field">
-            <label htmlFor="school">School:</label>
-            <input
-              type="text"
-              id="school"
-              name="school"
-              required="true"
-              spellCheck="false"
-              autoComplete="false"
-            />
+          <div className="form-field-wrapper">
+            <div className="form-field">
+              <label htmlFor="school">School:</label>
+              <input
+                type="text"
+                id="school"
+                name="school"
+                required="true"
+                spellCheck="false"
+                autoComplete="off"
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="name">Student name:</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                required="true"
+                spellCheck="false"
+                autoComplete="off"
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="indexNo">Index number:</label>
+              <input
+                type="number"
+                id="indexNo"
+                name="indexNo"
+                required="true"
+                spellCheck="false"
+                autoComplete="off"
+                onWheel={(e) => e.target.blur()}
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="dob">DOB:</label>
+              <input
+                type="date"
+                id="dob"
+                name="dob"
+                required="true"
+                spellCheck="false"
+                autoComplete="off"
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="english">English:</label>
+              <input
+                type="number"
+                id="english"
+                name="english"
+                required="true"
+                spellCheck="false"
+                autoComplete="off"
+                onWheel={(e) => e.target.blur()}
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="dzongkha">Dzongkha:</label>
+              <input
+                type="number"
+                id="dzongkha"
+                name="dzongkha"
+                required="true"
+                spellCheck="false"
+                autoComplete="off"
+                onWheel={(e) => e.target.blur()}
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="math">Mathematics:</label>
+              <input
+                type="number"
+                id="math"
+                name="math"
+                spellCheck="false"
+                autoComplete="off"
+                onWheel={(e) => e.target.blur()}
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="his">History:</label>
+              <input
+                type="number"
+                id="his"
+                name="his"
+                required="true"
+                spellCheck="false"
+                autoComplete="off"
+                onWheel={(e) => e.target.blur()}
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="geo">Geography:</label>
+              <input
+                type="number"
+                id="geo"
+                name="geo"
+                required="true"
+                spellCheck="false"
+                autoComplete="off"
+                onWheel={(e) => e.target.blur()}
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="eco">Economics:</label>
+              <input
+                type="number"
+                id="eco"
+                name="eco"
+                required="true"
+                spellCheck="false"
+                autoComplete="off"
+                onWheel={(e) => e.target.blur()}
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="result">Result:</label>
+              <select
+                name="result"
+                id="result"
+                required="true"
+                className="text-secondary"
+              >
+                <option value="">Result</option>
+                <option value="Pass certificate awarded">
+                  Pass certificate awarded
+                </option>
+                <option value="Pass certificate not awarded">
+                  Pass certificate not awarded
+                </option>
+              </select>
+            </div>
           </div>
-          <div className="form-field">
-            <label htmlFor="name">Student name:</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              required="true"
-              spellCheck="false"
-              autoComplete="false"
-            />
-          </div>
-          <div className="form-field">
-            <label htmlFor="indexNo">Index number:</label>
-            <input
-              type="number"
-              id="indexNo"
-              name="indexNo"
-              required="true"
-              spellCheck="false"
-              autoComplete="false"
-            />
-          </div>
-          <div className="form-field">
-            <label htmlFor="dob">DOB:</label>
-            <input
-              type="date"
-              id="dob"
-              name="dob"
-              required="true"
-              spellCheck="false"
-              autoComplete="false"
-            />
-          </div>
-          <div className="form-field">
-            <label htmlFor="english">English:</label>
-            <input
-              type="number"
-              id="english"
-              name="english"
-              required="true"
-              spellCheck="false"
-              autoComplete="false"
-            />
-          </div>
-          <div className="form-field">
-            <label htmlFor="dzongkha">Dzongkha:</label>
-            <input
-              type="number"
-              id="dzongkha"
-              name="dzongkha"
-              required="true"
-              spellCheck="false"
-              autoComplete="false"
-            />
-          </div>
-          <div className="form-field">
-            <label htmlFor="math">Mathematics:</label>
-            <input
-              type="number"
-              id="math"
-              name="math"
-              spellCheck="false"
-              autoComplete="false"
-            />
-          </div>
-          <div className="form-field">
-            <label htmlFor="his">History:</label>
-            <input
-              type="number"
-              id="his"
-              name="his"
-              required="true"
-              spellCheck="false"
-              autoComplete="false"
-            />
-          </div>
-          <div className="form-field">
-            <label htmlFor="geo">Geography:</label>
-            <input
-              type="number"
-              id="geo"
-              name="geo"
-              required="true"
-              spellCheck="false"
-              autoComplete="false"
-            />
-          </div>
-          <div className="form-field">
-            <label htmlFor="eco">Economics:</label>
-            <input
-              type="number"
-              id="eco"
-              name="eco"
-              required="true"
-              spellCheck="false"
-              autoComplete="false"
-            />
-          </div>
-          <div className="form-field">
-            <label htmlFor="result">Result:</label>
-            <select name="result" id="result" required="true">
-              <option value="">Result</option>
-              <option value="Pass certificate awarded">
-                Pass certificate awarded
-              </option>
-              <option value="Pass certificate not awarded">
-                Pass certificate not awarded
-              </option>
-            </select>
-          </div>
-          <button type="submit">Submit</button>
+          <button type="submit" className="marksheet-submit-btn">
+            Submit
+          </button>
         </form>
       </div>
     </div>
--- client/src/pages/Science.jsx
+++ client/src/pages/Science.jsx
@@ -3,134 +3,150 @@ import Sidebar from "../components/Sidebar";
 
 function Science() {
   return (
-    <div className="d-flex">
+    <div className="wrapper">
       <Sidebar />
-      <div>
-        <h1 className="d-block mx-auto mt-2">Class XII Science</h1>
+      <div className="d-flex flex-column justify-content-center align-items-center">
+        <h1 className="d-block mx-auto mt-2 mb-3 fs-2">Class XII Science</h1>
         <form className="marksheet-form">
-          <div className="form-field">
-            <label htmlFor="school">School:</label>
-            <input
-              type="text"
-              id="school"
-              name="school"
-              required="true"
-              spellCheck="false"
-              autoComplete="false"
-            />
+          <div className="form-field-wrapper">
+            <div className="form-field">
+              <label htmlFor="school">School:</label>
+              <input
+                type="text"
+                id="school"
+                name="school"
+                required="true"
+                spellCheck="false"
+                autoComplete="off"
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="name">Student name:</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                required="true"
+                spellCheck="false"
+                autoComplete="off"
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="indexNo">Index number:</label>
+              <input
+                type="number"
+                id="indexNo"
+                name="indexNo"
+                required="true"
+                spellCheck="false"
+                autoComplete="off"
+                onWheel={(e) => e.target.blur()}
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="dob">DOB:</label>
+              <input
+                type="date"
+                id="dob"
+                name="dob"
+                required="true"
+                spellCheck="false"
+                autoComplete="off"
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="english">English:</label>
+              <input
+                type="number"
+                id="english"
+                name="english"
+                required="true"
+                spellCheck="false"
+                autoComplete="off"
+                onWheel={(e) => e.target.blur()}
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="dzongkha">Dzongkha:</label>
+              <input
+                type="number"
+                id="dzongkha"
+                name="dzongkha"
+                required="true"
+                spellCheck="false"
+                autoComplete="off"
+                onWheel={(e) => e.target.blur()}
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="physics">Physics:</label>
+              <input
+                type="number"
+                id="physics"
+                name="physics"
+                required="true"
+                spellCheck="false"
+                autoComplete="off"
+                onWheel={(e) => e.target.blur()}
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="chemistry">Chemistry:</label>
+              <input
+                type="number"
+                id="chemistry"
+                name="chemistry"
+                required="true"
+                spellCheck="false"
+                autoComplete="off"
+                onWheel={(e) => e.target.blur()}
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="math">Mathematics:</label>
+              <input
+                type="number"
+                id="math"
+                name="math"
+                required="true"
+                spellCheck="false"
+                autoComplete="off"
+                onWheel={(e) => e.target.blur()}
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="bio">Biology:</label>
+              <input
+                type="number"
+                id="bio"
+                name="bio"
+                required="true"
+                spellCheck="false"
+                autoComplete="off"
+                onWheel={(e) => e.target.blur()}
+              />
+            </div>
+            <div className="form-field">
+              <label htmlFor="result">Result:</label>
+              <select
+                name="result"
+                id="result"
+                required="true"
+                className="text-secondary"
+              >
+                <option value="">Result</option>
+                <option value="Pass certificate awarded">
+                  Pass certificate awarded
+                </option>
+                <option value="Pass certificate not awarded">
+                  Pass certificate not awarded
+                </option>
+              </select>
+            </div>
           </div>
-          <div className="form-field">
-            <label htmlFor="name">Student name:</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              required="true"
-              spellCheck="false"
-              autoComplete="false"
-            />
-          </div>
-          <div className="form-field">
-            <label htmlFor="indexNo">Index number:</label>
-            <input
-              type="number"
-              id="indexNo"
-              name="indexNo"
-              required="true"
-              spellCheck="false"
-              autoComplete="false"
-            />
-          </div>
-          <div className="form-field">
-            <label htmlFor="dob">DOB:</label>
-            <input
-              type="date"
-              id="dob"
-              name="dob"
-              required="true"
-              spellCheck="false"
-              autoComplete="false"
-            />
-          </div>
-          <div className="form-field">
-            <label htmlFor="english">English:</label>
-            <input
-              type="number"
-              id="english"
-              name="english"
-              required="true"
-              spellCheck="false"
-              autoComplete="false"
-            />
-          </div>
-          <div className="form-field">
-            <label htmlFor="dzongkha">Dzongkha:</label>
-            <input
-              type="number"
-              id="dzongkha"
-              name="dzongkha"
-              required="true"
-              spellCheck="false"
-              autoComplete="false"
-            />
-          </div>
-          <div className="form-field">
-            <label htmlFor="physics">Physics:</label>
-            <input
-              type="number"
-              id="physics"
-              name="physics"
-              required="true"
-              spellCheck="false"
-              autoComplete="false"
-            />
-          </div>
-          <div className="form-field">
-            <label htmlFor="chemistry">Chemistry:</label>
-            <input
-              type="number"
-              id="chemistry"
-              name="chemistry"
-              required="true"
-              spellCheck="false"
-              autoComplete="false"
-            />
-          </div>
-          <div className="form-field">
-            <label htmlFor="math">Mathematics:</label>
-            <input
-              type="number"
-              id="math"
-              name="math"
-              required="true"
-              spellCheck="false"
-              autoComplete="false"
-            />
-          </div>
-          <div className="form-field">
-            <label htmlFor="bio">Biology:</label>
-            <input
-              type="number"
-              id="bio"
-              name="bio"
-              required="true"
-              spellCheck="false"
-              autoComplete="false"
-            />
-          </div>
-          <div className="form-field">
-            <label htmlFor="result">Result:</label>
-            <select name="result" id="result" required="true" >
-              <option value="">Result</option>
-              <option value="Pass certificate awarded">
-                Pass certificate awarded
-              </option>
-              <option value="Pass certificate not awarded">
-                Pass certificate not awarded
-              </option>
-            </select>
-          </div>
-          <button type="submit">Submit</button>
+          <button type="submit" className="marksheet-submit-btn">
+            Submit
+          </button>
         </form>
       </div>
     </div>
